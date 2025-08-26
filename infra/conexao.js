7dec50edcaacbcc839285eb76a934e1d6d584b9c
ADFS2050 <agcyborgdygital@gmail.com>
2025-08-26 18:39:10 -0300

    Criação do CRUD

--- infra/conexao.js
+++ infra/conexao.js
@@ -2,7 +2,7 @@ import mysql from 'mysql'
 
 const conexao = mysql.createConnection({
     host: '127.0.0.1' ,
-    port: '' ,
+    port: '3306' ,
     user:  'root' ,
     password: '',
     database: 'cursodb'
--- src/app.js
+++ src/app.js
@@ -4,13 +4,6 @@ const app = express()
 
 app.use(express.json())
 
-// const cursos = [
-//     {id: 1, disciplina: 'ADS'},
-//     {id: 2, disciplina: 'ADS'},
-//     {id: 3, disciplina: 'ADS'},
-//     {id: 4, disciplina: 'ADS'}
-// ]
-
 function buscarCursosPorId(id){
     return cursos.filter( curso => curso.id == id)
 }
@@ -19,14 +12,7 @@ function buscarIndexCurso(id){
     return cursos.findIndex( curso => curso.id == id)
 }
 
-//Criando um rota default (endpoint)
-// app.get('/',(req, res)=> {
-//      res.send('Hello World Senac')
-// }) 
-
-//ROTAS 
 app.get('/cursos' , (req, res)=> {
-    //res.status(200).send(cursos)
     const sql = "select * from curso;"
     conexao.query(sql, (error, result) =>{
         if (error) {
@@ -37,37 +23,54 @@ app.get('/cursos' , (req, res)=> {
     })
 }) 
 
-
-app.post('/cursos',(req, res)=> {
-    cursos.push(req.body)
-    res.status(200).send('Seleção cadastrada com sucesso!')
+app.get('/cursos/:id',(req, res)=> {
+    const { id } = req.params
+    const sql = "select * from curso where id = ?;"
+    conexao.query(sql, [id], (error, result) =>{
+        if (error) {
+            console.log(error)
+        } else {
+            res.status(200).json(result)
+        }
+    })
 }) 
 
-//Coletando informações do usuário
-app.get('/cursos/:id',(req, res)=> {
-    let index = req.params.id
-    console.log(index)
-})
+app.post('/cursos',(req, res)=> {
+    const curso = req.body
+    const sql = "insert into curso set ?;"
+    conexao.query(sql, curso, (error, result) =>{
+        if (error) {
+            console.log(error)
+        } else {
+            res.status(201).json(result)
+        }
+    })
+}) 
 
-//Buscando por id 
-app.get('/cursos/:id',(req, res)=> {
-    res.json(buscarCursosPorId(req.params.id))
-})
-
-//Deletando por id
-app.delete('/cursos/:id',(req, res)=> {
-    let index = buscarIndexCurso(req.params.id)
-    cursos.slice(index, 1)
-    console.log(index)
-    res.send(`O curso com id ${rep.parms.id} excluido com sucesso!`)
-})
-
-//Update por id
 app.put('/cursos/:id',(req, res)=> {
-    let index = buscarIndexCurso(req.params.id)
-    cursos [index].disciplina = req.body.disciplina
-    res.status(200).json(cursos)
-})
+    const { id } = req.params
+    const dados = req.body
+    const sql = "update curso set ? where id = ?;"
+    conexao.query(sql, [dados, id], (error, result) =>{
+        if (error) {
+            console.log(error)
+        } else {
+            res.status(200).json(result)
+        }
+    })
+}) 
+
+app.delete('/cursos/:id',(req, res)=> {
+    const { id } = req.params
+    const sql = "delete from curso where id = ?;"
+    conexao.query(sql, [id], (error, result) =>{
+        if (error) {
+            console.log(error)
+        } else {
+            res.status(200).json(result)
+        }
+    })
+}) 
 
 
 export default app
